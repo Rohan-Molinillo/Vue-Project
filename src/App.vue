872d016a886dd55554564eedddf7a5f4cf1aa6af
Rohan <rohanmolinillo@gmail.com>
2023-02-24 15:02:52 +0100

    First static restaurant component

--- src/App.vue
+++ src/App.vue
@@ -15,5 +15,9 @@ export default {
 </script>
 
 <style>
+@import url('./style/reset.css');
 @import url('https://fonts.googleapis.com/css2?family=Roboto%3Awght%40300%3B400%3B700&display=swap');
+#app {
+  font-family: 'Roboto', sans-serif;
+}
 </style>
--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="restaurant-card">
-    <p>test</p>
+    <div class="restaurant-img"></div>
+    <div class="restaurant-info">
+      <div class="top">
+        <p class="name">Subway</p>
+        <p class="note">
+          <span> 4.5 </span>
+        </p>
+      </div>
+      <p class="time">20-30min</p>
+    </div>
   </div>
 </template>
 
@@ -15,6 +24,36 @@ export default {}
 
   p {
     margin: 0px;
+  }
+  .restaurant-img {
+    background-image: url('https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8zNzg4MDJiMC1jNTI4LTQ4MjktYjBiNS0wY2M2NDBkZjYzY2QuanBlZw==');
+    height: 70%;
+    background-size: cover;
+    background-position: center;
+  }
+
+  .restaurant-info {
+    .top {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin: 10px 0px;
+
+      .note {
+        background-color: #eee;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 30px;
+        width: 30px;
+        font-size: 0.8rem;
+        border-radius: 50%;
+      }
+    }
+
+    .time {
+      font-size: 0.9rem;
+    }
   }
 }
 </style>
