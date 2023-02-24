912fe332fbc0a604fa96b03da39353ac14a0a3ff
Rohan <rohanmolinillo@gmail.com>
2023-02-24 11:29:33 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <RestaurantCard />
+  </header>
+</template>
+
+<script>
+import RestaurantCard from './components/RestaurantCard.vue'
+export default {
+  name: 'App',
+  components: {
+    RestaurantCard
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto%3Awght%40300%3B400%3B700&display=swap');
+</style>
