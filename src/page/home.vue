30d245da93af9b9c403b8a78974670c806b97e34
Rohan <rohanmolinillo@gmail.com>
2023-02-28 09:34:26 +0100

    creation home page and constructor for bdd

--- src/page/home.vue
+++ src/page/home.vue
@@ -7,6 +7,7 @@
 <script>
 //IMPORT
 import BDD from '../BDD.js'
+import { onMounted } from 'vue'
 
 //COMPONENTS
 import RestaurantRow from '../components/RestaurantRow.vue'
@@ -25,7 +26,29 @@ export default {
       }
     }
 
-    const resto = new Restaurant('Mcdo', 4.8, 'img', '45min')
+    let dataRestaurant = []
+
+    const makeDataRestaurant = () => {
+      let three_restaurant = []
+
+      for (const restaurant of BDD) {
+        const new_restaurant = new Restaurant(
+          restaurant.name,
+          restaurant.note,
+          restaurant.image,
+          restaurant.drive_time
+        )
+
+        if (three_restaurant.length === 2) {
+          three_restaurant.push(new_restaurant)
+          dataRestaurant.push(three_restaurant)
+          three_restaurant = []
+        } else {
+          three_restaurant.push(new_restaurant)
+        }
+      }
+    }
+    onMounted(makeDataRestaurant)
   }
 }
 </script>
